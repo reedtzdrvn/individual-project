bf7ff64e8f6030d288ab2b0327080141fb8acf1e
Daniel Markov <reedtzhere@gmail.com>
2022-03-23 19:47:58 +0300

    Updates

---
+++ precache-manifest.845cecf3acbeeb827b9b972253f0376e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c6744a7e5a9a20e3c9eee99c51dd50",
+    "url": "/individual-project/index.html"
+  },
+  {
+    "revision": "1b1fa015e79e90936666",
+    "url": "/individual-project/static/css/main.859dc16a.chunk.css"
+  },
+  {
+    "revision": "c70a6eb08fc9e6387365",
+    "url": "/individual-project/static/js/2.b7d753a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/individual-project/static/js/2.b7d753a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1fa015e79e90936666",
+    "url": "/individual-project/static/js/main.af70dbc4.chunk.js"
+  },
+  {
+    "revision": "25074b5c1ffddd60d2f9",
+    "url": "/individual-project/static/js/runtime-main.2ddbb261.js"
+  },
+  {
+    "revision": "712fe5c8fc91628f0b50d0852a3efa26",
+    "url": "/individual-project/static/media/background.712fe5c8.jpg"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/individual-project/static/media/cross.5fe0db66.svg"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/individual-project/static/media/vector.6cb6e5fc.svg"
+  }
+]);
